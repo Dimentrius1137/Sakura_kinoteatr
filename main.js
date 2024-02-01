cccfaf7ba9ccf2e504a0ebd6260341f04dca4862
Dimentrius <lavrentevd579@gmail.com>
2024-02-01 03:48:55 +0300

    Сделано: Адаптивная сетка карточек, левый навбар, дроп меню

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-let _none_poster = "https://www.kinopoisk.ru/1a4Q3q241/000899BY/tlp1TqWwQYiK-KwM_vly8t34nK_zWQrXc_C0_D3YeIcTNgOmyX-d1nXNhGE2a32KZqpp0XaHvOsfAf0d6JTAeacqNIollHDo6z0dHX55dkK9WRgel0kbVoNjtg7fW2vBMHWxJLrevEdF3QQBBkqNniarnSEXRHvvjsKZ_Xn1EFknqDV4o4YSmTkoro19aHSgjIsJfdfse_eDoCT8v8pbcRAnEDT5fO3nQg11MQUZ31_W2zvhN5dJWp7D676YJXCYxEkGG-PHkku4Sb6trVhk0J67ir-FbdpWsdE3rD3qeBJSV_FkuJyPFOJPFvMFOuwtNIgpUyalKgqM4Wqd2iQAuGYb5fkRZ9Fou_jenqwYFsMMOYvNVU775THQFdxt6TkzMaRwB6jeTUUTnQTTZdrOPcUpufJG9Uior3Oq3JllUll2mNQbU5ZhKKiq34_tSYbxTrrqjjd9WTRiIPUOvRvpIMO1A9e4ru4Ggw90wtb67x5kmhmgpXf7Gc9zuH4aJHD75Io06TNUYHopGuy9DOvnUX77-M4n75s1Q0K3vIw5qMEAZnHkau_dtnKtNbHWKA4M1ouJkFeEKJoP4zk-S6YQWOcpJImDdxMJmwseXW9bxJKOq1le1p7pZQADVs6cCFkQ0kVwJ3oP_FegPKURtflPnhdoqNCmtqsZPrM4ninmguvWCzVochZzCis4nu_taHaQjXk4LIfvuHbAgmUfXOsZsrIEs0SaHy600wyHMGcY_8x0-5iwZxaKue8w2owLdxDKpJnmC_NWYMpIuT09PijnMR_ru26Wr2rlcVLVrO-bi2FSxePk2h0uZHBvBeBGaR--phqIQKY1Wxv-oiq_u1WiyQQ6NkphNlBJqwldDa7ZFIDeqnn8JF-Yd7NhBM49GYgRozRjBkpdjfaQf2XjJYq_L6UaapMnh_sL_sDLflk1c4sXC5RrMAbi-9r5Tp8MGhZxDUs6n1b8eDXCstePrdsKI7GH8_eLTt-GoC-X0ISI_l6WOEkSZVVqqezTK2y41XFJVIumqZDkgtn5Km28LBgkYk3r-n4F7Vh006Dl7g9p6mFBdVBXKG7tF3BuJcGUW02-ZztLsKdFKotfQAjPmyUy61VpZhtxN9KYatj-fxyrtGFcKqhOh016hXHAlL_uGsuzoSfSZAq-_nUBLKeg5uoPvRapqQNllapLv1DrL_lmwKrkekV7UuRDKsk6jj_8SdZxHRhaDcQ_CkWTkKcO_VuKQ_JH8WaLD_5mwr010IWor4_0G3mDV4U5OI8DezxJF5OpNUhGq3J1gyub-9yN_Fg0cJ-LOGyVLaoVYkKkfazq2VHyFlL2yN3dpHIcpDMXyQxMRvmp8zQEubjP8tu_miUCiwQ65mtxlYD7C4uPvvzbhOJfeat_Vp5ZVvCQJl1fa5pSUHcx94j9rpRR71QjpAqOH5Q7GtGkdOo4_WAa78tFEFiFOkQ6YkWBGYhLPO69SiRxfilqz6X9SSbTIgcurSt7QPBX8harro6mcCy10KY6zX6WyCkg1Ga4iqxzSyx61PAYdchFeCA2oyopKM9crJm0Y1woSQ_0zRkG89DFbIxJ2VCxl-Bkyp0_5NCfhREEiA49xPv6wzaWqWvdQ9s-aReRe4QqJKoDpaK5Cdtuz044VsBtWEruZA3otHBTNS4N-hlhQdcz9Rkdf_dCLNWQtcu9Xxeoa6B292gpzYDbjesVc9h3ikbZUFfxmCt7H57MGlYC_3kaLlfMWcdSYPUu_MoaA3HWA2RY7N_mkQzmI6bLXk_3u4kBttbqmoyjyK3LdLK4p2pHaXNl4Qub6c9M3FmFQL0JW85VHvsWkUHXPp4ZK1NxxHIEmU--BkGPdxD22M385Jmo8EVU2TqusOkt6uZx-GSYFrhwNsKbmks_XD6LZkAP6Ihs9y24dwJCZOysKVjgUaVjRxhM_HVT7efhlRvfr9X5qsKXFimKDfE5PEiVYZjHiya4cFbBu6krfA2tScYgb3t53Jdt6QbwUNdOnNuJkvM2cHb7vux3Yw32ExQZvQxHSrswB3X5uC6BS0yahMOYlAmUCDGm8FjZCV69LTp2QJ6JCv4U_skUceIFDm-raTKwZWOnCl5NxSGPNEGWmNzuR7sJ8WdnGsls4BqMqYcy-Sa4Vslg57F4OqvPTr9rNABNulo9ZDyL9vGiNz4fO8khUxeC1todPncib-aBVSiObcaKqkNmh5g6r0AY_5nGEgjXmubKUmfCmTv7j06PWAdinCrpPmbfmWWxIoeM_mgpcZMFYlS4vl13YZ3FsxeLL4wm-wjS5OcqK0_y2Mx5B3HoV-o2O2FlEkvamv0PPuv0owy6STyEf-i2wGHVLd4ZmSFjFqMUGOyPd6FvpcNWqr_eBjioo2Rn-TqeQIsMqNQS6xWpNssh9uO6WyltDC94ZVBcCkrNtU86tbPSFY7eKHqB0kRhhMju_nczHpRjp8ldffdJqLDUxLtq_bLbrGsmgui1qhSoY2QxuApYft3N6gTDv2sofOduizXxYRbfjCrJINMXk3eJHSx1Y22lgRYr79yFSEsyJmUYqa1S-zxbNqOpJBu1CHEEQqh5mR9N_gpHQK-Kmk-FHvn0sHNnPb2KavDRlQPEaZzOp5OeFhC0KX7ttTt60Pa0y4meUhmcySdBS8XaNxni5cBaKLq8TM0aZKBs6zjdtF8p5LCQVMw_CFihceRy13idjRaTnLWDZsucDJYZqdMW5AkpnMK5_BjmE8mkO2dKcwTwiYm5_yyf6zaxrKi6bZVPyUdzUWaOX1pLIPEF4zVojf30cn9FgTUZ_ZyHadmS1qTK-d1wuM2JFUG75wvUCFAEUOg5Gd4vTKgXQC1rSuxknWv1sfC3bE87esCCRdGU6RyNdtBtBEFlq53sh4nKUga1WGnuwdlNyRcR-aeJJVtyJjNqCSk9XezpVOAMqkhO9T9rFcGy942_mjszoGaw9FqPTGQCndTBxgs-TZeKOTOlZ2qbzsII8";
+let _none_poster = "interface items/none_pic.png"
 const catalog = document.querySelector('.catalog');
 const moreBtn = document.querySelector('.more');
-const url = "https://api.kinopoisk.dev/v1.4/movie/random?typeNumber=4&year=2014";
+const navbar = document.querySelector('.navbar');
+const navbarBtn = document.querySelector('.open_close_navbar');
+const navbarCont = document.querySelector('.navbar-cont');
+const arrow = document.querySelector('.top-arrow');
+const dropMenu = document.querySelector('.list-hide');
 
-async function CreateCards()
+function Hide_DropMenu()
+{
+    dropMenu.classList.toggle("list");
+    arrow.classList.toggle("top-arrow-up");
+}
+
+function CreateCards()
 {
 
     const newCard = document.createElement('div');
@@ -18,7 +28,10 @@ async function CreateCards()
 
 //будет задаваться функцией состояния окна в зависиости от разрешения
 let itemsCount = 6;
-
+if(window.innerWidth > 1600)
+{
+    itemsCount = 8;
+}
 document.addEventListener('DOMContentLoaded', function()
 {
     for(let i = 0; i < itemsCount; i++)
@@ -29,39 +42,59 @@ document.addEventListener('DOMContentLoaded', function()
 })
 
 let rows = 2;
-moreBtn.addEventListener('click', function()
+function AddCards()
 {
-    if(document.documentElement.getBoundingClientRect().bottom < document.documentElement.clientHeight + 150)
+    
+    if(window.innerWidth >= 1280 && document.documentElement.getBoundingClientRect().bottom < document.documentElement.clientHeight + 150)
     {
         rows+= 2;
+        console.log(rows)
         catalog.style.gridTemplateRows = `repeat(${rows}, 400px)`
        for(let i = 0; i < itemsCount; i++)
         {
             CreateCards();
         }
+
         GetData();
     } 
 
     //Костыльная хуета
-    if(window.innerWidth < 1280 && document.documentElement.getBoundingClientRect().bottom > document.documentElement.clientHeight - 150)
+    else if(window.innerWidth < 1280 && document.documentElement.getBoundingClientRect().bottom > document.documentElement.clientHeight - 200)
     {
-        rows+= 3;
-        catalog.style.gridTemplateRows = `repeat(${rows}, 400px)`
+        
+        rows+= 2;
+        console.log(rows)
+        catalog.style.gridTemplateRows = `repeat(${rows}, 160px)`
        for(let i = 0; i < itemsCount; i++)
         {
             CreateCards();
         }
+
         GetData();
     }
-    // if(rows > 12)
-    // {
-    //     this.style.display = "none";
-    // }
- 
-    
-})
+}
 
 
+//навбар
+function NavbarState()
+{
+    navbarCont.classList.toggle('navbar-cont-on');
+}
+navbarBtn.addEventListener('click', NavbarState)
+
+function NavScroll()
+{
+    navbar.classList.add("if_page_scroll")
+    if(window.scrollY == 0)
+    {
+        navbar.classList.remove("if_page_scroll")
+    }
+}
+document.addEventListener('scroll', NavScroll)
+
+//получение данных
+
+const url = "https://api.kinopoisk.dev/v1.4/movie/random?typeNumber=4&year=2014";
 async function GetData()
 {
     const card = document.querySelectorAll('.card');
@@ -73,7 +106,6 @@ async function GetData()
         {
             const data = await fetch(url, { headers: { 'X-API-KEY': 'WR46T4C-A2MMNGP-MX8DMH3-A160B0X' } });
             const films = await data.json();
-
             if(films.poster.url != null)
             {
                 card[i].children[0].src = films.poster.url
@@ -91,6 +123,8 @@ async function GetData()
 
         }
         catch(er){
+            card[i].children[0].src = _none_poster;
+            card[i].children[1].innerHTML = "none";
             console.log(`error: ${er}`)
         }
     }
